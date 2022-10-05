06167625000d3246ec944feb973d4025a61b99f1
MichaelBinnick <mbinnick@live.ca>
2022-10-05 10:17:36 -0400

    implements InterviewList.js component with dynamic rendering of interviewerlistitem components from array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import "components/InterviewerList.scss"
+import InterviewerListItem from './InterviewerListItem';
 
 export default function InterviewerList(props) {
+  
+  const interviewerListItems = props.interviewers.map(viewer => {
+    return <InterviewerListItem
+      key={viewer.id}
+      avatar={viewer.avatar}
+      name={viewer.name}
+      selected={viewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />
+  })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewerListItems}
+      </ul>
     </section>
   )
 }
