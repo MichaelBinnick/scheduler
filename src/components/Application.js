8f5cec37acfe1ef1e2340d60c059d026406ce316
MichaelBinnick <mbinnick@live.ca>
2022-10-06 13:49:22 -0400

    removes old hardcoded days array from Application.js on confirmation that axios get request works

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,23 +45,6 @@ const appointments = {
 };
 
 export default function Application(props) {
-  // const days = [
-  //   {
-  //     id: 1,
-  //     name: "Monday",
-  //     spots: 2,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Tuesday",
-  //     spots: 5,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Wednesday",
-  //     spots: 0,
-  //   },
-  // ]
 
   const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
