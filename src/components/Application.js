f96421548e07eb0bd21d9d68cd61eb851a03b098
MichaelBinnick <mbinnick@live.ca>
2022-10-06 13:47:36 -0400

    implements axios get request to API server to populate days array for Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,25 +45,34 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ]
-  
+  // const days = [
+  //   {
+  //     id: 1,
+  //     name: "Monday",
+  //     spots: 2,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Tuesday",
+  //     spots: 5,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Wednesday",
+  //     spots: 0,
+  //   },
+  // ]
+
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then((res) => {
+        setDays(res.data)
+      })
+      .catch(e => console.log(e))
+  }, [])
 
   return (
     <main className="layout">
