ecf76b8e91e48312529b0c97609b5ea097401691
MichaelBinnick <mbinnick@live.ca>
2022-10-10 13:20:54 -0400

    implements delete appointment logic, axios request

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -13,6 +13,7 @@ export default function Appointment(props) {
   const SHOW = 'SHOW';
   const CREATE = 'CREATE';
   const SAVING = 'SAVING';
+  const DELETING = 'DELETING';
 
   const { mode, transition, back } = useVisualMode(
       props.interview ? SHOW : EMPTY
@@ -30,6 +31,16 @@ export default function Appointment(props) {
       })
   }
 
+  const cancel = function() {
+    
+    transition(DELETING);
+    props.cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+  }
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -38,10 +49,12 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={cancel}
         />
       )}
       {mode === CREATE && <Form interviewers={[...props.interviewers]} onCancel={back} onSave={save} />}
       {mode === SAVING && <Status message="Saving..." />}
+      {mode === DELETING && <Status message="Deleting..." />}
     </article>
   );
 }
