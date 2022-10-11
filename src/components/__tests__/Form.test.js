d067a8c07ffb949b76b5c370861e7681aeab0e3a
MichaelBinnick <mbinnick@live.ca>
2022-10-11 14:05:06 -0400

    finishes Form tests, implements error prevention for empty student/interviewer, gets tests to pass

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -31,7 +31,7 @@ describe("Form", () => {
     const onSave = jest.fn();
 
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
-    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} student="" />);
+    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} />);
     
     /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
@@ -45,7 +45,7 @@ describe("Form", () => {
     const onSave = jest.fn();
 
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
-    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} interviewer={null} />);
+    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} student="Lydia Miller-Jones" />);
 
     /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
